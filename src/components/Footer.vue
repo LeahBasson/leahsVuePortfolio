6bbccb01c7caf863664136146f9e840e148fc4b9
LeahBasson <leahbasson54@gmail.com>
2024-07-01 09:41:01 +0200

    nav & footer :technologist:

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="container-fluid">
+        <div class="row">
+            <p class="lead text-white">
+                <span class="shadow">Leah's Portfolio</span>
+                <span>&copy; 2024</span>
+            </p>
+        </div>
+      </footer>
+  </template>
+  
+  <script>
+  export default {
+      name: "FooterComp",
+      data(){
+          return{
+              cYear: new Date().getUTCFullYear()
+          }
+      }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav class="navbar sticky-top navbar-expand-lg bg-black navbar-dark">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#home"><img class="img-fluid" src="https://leahbasson.github.io/MyImages/images/portfolioLogo.png" alt="logo" loading="lazy"></a>
+            <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#resume">Resume</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#skills">Skills</a>
+                </li>
+                <li class="nav-item"> 
+                  <a class="nav-link" href="#projects">Projects</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#testimonials">Testimonials</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#reachMe">Reach me</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+    </template>
+    
+    <script>
+    export default {
+        name: "NavComp"
+  
+        
+    }
+    
+    </script>
+    
+    
